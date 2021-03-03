3ff8bebede7c001556af9ed7d57380efbcfc55e3
jaimdedan <f230fffd-b99b-4112-b9a7-ae408c42b53d@anonaddy.me>
2021-03-03 12:40:54 -0500

    add user's test, generate events model

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root 'events#index'
+  resources :user, only: [:new, :create, :show]
+  resources :event
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
