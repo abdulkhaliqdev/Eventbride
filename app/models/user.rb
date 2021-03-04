3d47ffe0305c6f7b30da27f5a0af195b4150880d
jaimdedan <f230fffd-b99b-4112-b9a7-ae408c42b53d@anonaddy.me>
2021-03-04 17:45:28 -0500

    delete destroy in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   validates :name, presence: true
   validates_uniqueness_of :name
-  has_many :created_event, foreign_key: 'creator_id', class_name: 'Event', dependent: destroy
+  has_many :created_events, foreign_key: 'creator_id', class_name: 'Event'
 end
