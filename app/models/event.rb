6f5ad81fc5c9fc7c92717ccfadf9ad19e6c3938c
abdulkhaliqdev <abdulkhaliql1f18bscs0159@gmail.com>
2021-03-04 19:47:09 +0500

    Add rspec test for user and events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,9 @@
 class Event < ApplicationRecord
+
+  validates_presence_of :event
+  validates_presence_of :description
+  validates_presence_of :date
+  validates_presence_of :user_id
+
   belongs_to :user
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { Event.new(event: 'Ruby on rails', description: 'Event for ruby on rails',date: DateTime.now + 1.week, user_id: 1 ) }
+  context 'validation of Event' do
+    it 'Event is valid with valid attributes' do
+      user { User.find(1) }
+      expect(subject).to be_valid
+    end
+    it 'rejects if tile not present' do
+      subject.event = nil
+      subject.description = 'Event arrange for Ruby on Rails'
+      subject.date = '2021-04-20'
+      subject.user_id = '1'
+
+      expect(subject).to_not be_valid
+    end
+  end
 end
