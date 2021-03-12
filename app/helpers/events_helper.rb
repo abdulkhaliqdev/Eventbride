42bc120ca39cb6fa5621eb59b4e0180091a05858
abdulkhaliqdev <abdulkhaliql1f18bscs0159@gmail.com>
2021-03-12 22:17:11 +0500

    shift view logic to helper methods and remove rubocop linters errors

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -3,17 +3,23 @@ module EventsHelper
     render 'events/create_event_button' if user_signed_in?
   end
 
-  def check_attendee(current_user, attendee, flag)
-    flag = true if current_user == attendee
+  def check_attendee(current_user, attendee)
+    return true if current_user == attendee
   end
 
-  def attendances_method
-     render 'events/attendance_form'
-    # if flag == true
-    # render 'events/cancelattendance'
-    # else
-    #   if user_signed_in? && current_user.id != event.creator_id
-    #   end
-    # end
+  def attendances_method(flag, current_user, event)
+    if flag == true
+      render 'events/cancelattendance'
+    elsif user_signed_in? && current_user.id != event.creator_id
+      render 'events/attendance_form'
+    end
+  end
+
+  def create_partical(current_user, event)
+    if user_signed_in? && current_user.id == event.creator.id
+      render 'events/just_you'
+    else
+      render 'events/creator_name'
+    end
   end
 end
